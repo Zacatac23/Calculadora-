6bfeab5960eb82dc4a3b3488391507c916aada3e
Zacatac23 <zac231104@uvg.edu.gt>
2024-07-30 17:53:18 -0600

    Main

--- Calculadora/src/main/kotlin/Main.kt
+++ Calculadora/src/main/kotlin/Main.kt
@@ -3,14 +3,26 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val calculator = ScientificCalculator()
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val expressions = listOf(
+        "3 + 5",
+        "10 - 2 * 5",
+        "(1 + 2) * (3 + 4)", // Resultado esperado: 21
+        "3 ^ 2 + 4 ^ 2",     // Resultado esperado: 25
+        "2 * (3 + 5) / 4",   // Resultado esperado: 4.0
+        "2 * ((3 + 5) / 4)", // Resultado esperado: 4.0
+        "((2 + 3) * 2) + 5", // Resultado esperado: 15
+        "(2 + 3) * (2 + 5)", // Resultado esperado: 35
+        "3 / (3 - 3)"        // Debería lanzar una excepción de división por cero
+    )
+
+    for (expr in expressions) {
+        try {
+            val result = calculator.evaluate(expr)
+            println("Resultado de '$expr': $result")
+        } catch (e: Exception) {
+            println("Error al evaluar '$expr': ${e.message}")
+        }
     }
 }
